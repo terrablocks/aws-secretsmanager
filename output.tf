4beaa877fe7d85ba573c268882d1628c8902ff55
Vimal Paliwal <vimalpaliwal.me@gmail.com>
2022-06-04 18:16:03 +0530

    update output names

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
-output "secret_arn" {
+output "arn" {
   value       = local.secret_id
   description = "ARN of the secret"
 }
 
-output "secret_version" {
+output "version_id" {
   value       = aws_secretsmanager_secret_version.this.version_id
   description = "Version ID of the secret"
 }
